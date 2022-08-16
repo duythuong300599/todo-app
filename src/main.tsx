783292ecc762a6283774a78c35638c0b0ff7cd3d
DuyT <duythuong300599@gmail.com>
2022-08-16 10:50:36 +0700

    setup i18next and store in project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { StoreProvider } from "./stores";
+import i18n from "./translations/i18n";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </StoreProvider>
   </React.StrictMode>
-)
+);
