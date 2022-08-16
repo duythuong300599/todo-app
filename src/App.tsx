e0d9a72de208cfed0d36928778342726ef105d07
DuyT <duythuong300599@gmail.com>
2022-08-16 11:23:47 +0700

    custom hook and comment i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useStore } from "./stores";
 
 function App() {
   const [count, setCount] = useState(0);
+  const state = useStore();
+  console.log(state);
 
   return <></>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,9 @@ import i18n from "./translations/i18n";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <StoreProvider>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
+      {/* <I18nextProvider i18n={i18n}> */}
+      <App />
+      {/* </I18nextProvider> */}
     </StoreProvider>
   </React.StrictMode>
 );
